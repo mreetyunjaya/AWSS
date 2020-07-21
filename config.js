8dd447573ff4374ef43fddc8a5384db2b1111055
Tarık Seyceri <tito781227@hotmail.com>
2020-07-21 16:21:17 +0300

    Update

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	// Global Configs
-	THIS_PROJECT_FOLDER_NAME: "AWSS", // Change it only if you changed this project's folder name, // case sensitive // Used in core/index.js
+	THIS_PROJECT_FOLDER_NAME: "AWSS", // Change it only if you changed this project's folder name, // case sensitive // for Contributors (Used in core/index.js)
 	IS_DEBUG_MODE_ENABLED: false, // For Debugging this Toolkit
 	
-	DEFAULT_PROJECT_PATH_TO_SCAN: "../online-shopping-system-master/", // `../` means one level up from the toolkit's folder, you can replace it with full path such as C:\\Users\\YourUsername\\Desktop\\ProjectToScan
-	USED_PROGRAMMING_LANGUAGE: "php", // choose php or nodejs maybe // not empty! // Used in main.js // for now only PHP supported
-	HTML_REPORT_FILE_NAME_AND_PATH: "report.html", // default path is next to the main.js of the toolkit // you can specify full path such as C:\\Users\\YourUsername\\Desktop\\report.html
+	USED_PROGRAMMING_LANGUAGE: "php", // choose php // not empty! // for now only PHP supported // for Contributors (Used in main.js)
+	DEFAULT_PROJECT_PATH_TO_SCAN: "../ProjectToScan/", // `../` means one level up from the toolkit's folder, you can replace it with full path such as C:\\Users\\YourUsername\\Desktop\\ProjectToScan\\ (on windows) or /home/YourUsername/ProjectToScan/ (on Linux) or /Users/YourUsername/Desktop/ProjectToScan/ (on MacOSX)
+	HTML_REPORT_FILE_NAME_AND_PATH: "report.html", // default path is next to the main.js of the toolkit // you can specify full path such as C:\\Users\\YourUsername\\Desktop\\report.html (on windows) or /home/YourUsername/report.html (on Linux) or /Users/YourUsername/Desktop/report.html (on MacOSX)
 };
--- config_php_lang.js
+++ config_php_lang.js
@@ -1,21 +1,21 @@
 module.exports = {
-	PHP_EXE_BIN_PATH: "C:\\xampp\\php\\php.exe", // You should have PHP Binary installed somewhere, XAMPP works! // leave it empty "" if not set (if you don't have PHP binary)
+	PHP_EXE_BIN_PATH: "C:\\xampp\\php\\php.exe", // You should have PHP Binary installed somewhere, XAMPP works! // leave it empty "" if not set (if you don't have PHP binary) \\ XAMPP PHP Binary for linux is located in /opt/lampp/bin/php \\ XAMPP PHP Binary for MacOSX is located in /Applications/XAMPP/bin/php
 	
 	// is web app using DBMS?
 	IS_DBMS_USED: true, // true or false // DBMS System must be installed, for an example: for MySQL, XAMPP works great.
-	DBMS: "mysql", // This is important // might use PostgreSQL or other DBMS, not implemented for now // only MySQL Supported for Now!
+	DBMS: "mysql",  // only MySQL Supported for Now! // might use PostgreSQL or other DBMS in the future but not implemented for now.
 	
 	// if above IS_DBMS_USED = true, bellow settings are enabled and must be set
-	YOUR_WEBAPP_DBMS_SERVER_IP: "localhost",
-	YOUR_WEBAPP_DBMS_DB_NAME: "online-shopping-system",
+	YOUR_WEBAPP_DBMS_SERVER_IP: "127.0.0.1", // or localhost // or other ip
+	YOUR_WEBAPP_DBMS_DB_NAME: "hostel_management_system",
 	YOUR_WEBAPP_DBMS_USERNAME: "root",
 	YOUR_WEBAPP_DBMS_PASSWORD: "",
 	
-	YOUR_WEBAPP_DBMS_USER_TABLE_NAME: "user_info",
-	YOUR_WEBAPP_DBMS_USER_TABLE_PASSWORD_COLUMN_NAME: "password",
-	YOUR_WEBAPP_DBMS_USER_TABLE_SALT_COLUMN_NAME: "", // if not existed or used bcrypt or don't know what it is, leave it empty text: ""
+	YOUR_WEBAPP_DBMS_USER_TABLE_NAME: "student",
+	YOUR_WEBAPP_DBMS_USER_TABLE_PASSWORD_COLUMN_NAME: "pwd_hash",
+	YOUR_WEBAPP_DBMS_USER_TABLE_SALT_COLUMN_NAME: "salt", // if not existed or used bcrypt or don't know what it is, leave it empty text: ""
 
-	YOUR_WEBAPP_DBMS_ADMIN_TABLE_NAME: "admin_info", // maybe same of user table, so write same table name // if hardcoded admin credentials or doesn't exist, leave it empty text: ""
-	YOUR_WEBAPP_DBMS_ADMIN_TABLE_PASSWORD_COLUMN_NAME: "admin_password", // maybe same of user table, so write same column name
-	YOUR_WEBAPP_DBMS_ADMIN_TABLE_SALT_COLUMN_NAME: "", // if not existed or used bcrypt or don't know what it is, leave it empty text: "" // maybe same of user table, so write same column name
+	YOUR_WEBAPP_DBMS_ADMIN_TABLE_NAME: "admin", // maybe same of user table, so write same table name // if hardcoded admin credentials or doesn't exist, leave it empty text: ""
+	YOUR_WEBAPP_DBMS_ADMIN_TABLE_PASSWORD_COLUMN_NAME: "pwd_hash", // maybe same of user table, so write same column name
+	YOUR_WEBAPP_DBMS_ADMIN_TABLE_SALT_COLUMN_NAME: "salt", // if not existed or used bcrypt or don't know what it is, leave it empty text: "" // maybe same of user table, so write same column name
 };
